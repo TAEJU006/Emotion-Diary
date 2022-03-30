d5f97876393e9b50d095d00c7fc7f57df4ccc0fb
TAEJU <TAEJU@naver.com>
2022-03-30 18:47:31 +0900

    home의 header 구현

--- src/App.js
+++ src/App.js
@@ -19,13 +19,11 @@ const reducer = (state, action) => {
       break;
     }
     case "REMOVE": {
-      // targetId를 필터링한 나머지 요소들을 배열로 만들어서 newState로 바꾼다
       newState = state.filter((it) => it.id !== action.targetId);
       break;
     }
     case "EDIT": {
       newState = state.map((it) =>
-        // action.data.id가 일치하는 요소를 찾아낸 다음, 일치하는 요소에는 action.data를 전달함
         it.id === action.data.id
           ? {
               ...action.data,
@@ -37,7 +35,7 @@ const reducer = (state, action) => {
     default:
       return state;
   }
-  // 변화한 newState를 반환해 상태 적절히 바뀐다
+
   return newState;
 };
 
@@ -48,8 +46,7 @@ function App() {
   const [data, dispatch] = useReducer(reducer, []);
 
   const dataId = useRef(0);
-  //CREATE
-  // data,content,emotion을 받아서 새로운 일기 아이템을 객체로 만들어 data로 전달
+
   const onCreate = (date, content, emotion) => {
     dispatch({
       type: "CREATE",
@@ -70,7 +67,7 @@ function App() {
   const onEdit = (targetId, date, content, emotion) => {
     dispatch({
       type: "EDIT",
-      // Id는 유지를 하면서 나머지 파라미터를 바꾼다
+
       data: {
         id: targetId,
         date: new Date(date).getTime(),
@@ -81,9 +78,7 @@ function App() {
   };
 
   return (
-    //context.provider가 컴포넌트 트리를 전역으로 감싸고 있다
     <DiaryStateContext.Provider value={data}>
-      {/* dispatch 함수들도 context를 이용해 공급 */}
       <DiaryDispatchContext.Provider value={(onCreate, onRemove, onEdit)}>
         <BrowserRouter>
           <div className="App">
--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -2,7 +2,6 @@ const MyButton = ({ text, type, onClick }) => {
   const btnType = ["positive", "negative"].includes(type) ? type : "default";
   return (
     <button
-      // className은 문자열로 전달해야 해서 배열로 전달하면 안된다 => join 메서드를 통해서 띄어쓰기를 separate로 해서 합쳐줌
       className={["MyButton", `MyButton_${btnType}`].join(" ")}
       onClick={onClick}
     >
